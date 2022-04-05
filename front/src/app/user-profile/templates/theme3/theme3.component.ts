cce1951d9797621f0f7b2ff7cbac88fcbd1736d9
Szumi <patryk.szumilo324@gmail.com>
2022-04-05 10:48:02 +0200

    'theme3'

---
+++ front/src/app/user-profile/templates/theme3/theme3.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-theme3',
+  templateUrl: './theme3.component.html',
+  styleUrls: ['./theme3.component.css']
+})
+export class Theme3Component implements OnInit {
+
+  @Input() userDetails;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
